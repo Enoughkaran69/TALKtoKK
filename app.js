191b73f91038a7f5251eb1132efb3e7f28809ad0
Enoughkaran69 <138587207+Enoughkaran69@users.noreply.github.com>
2024-11-25 13:27:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// Import and configure Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBhT_v5ZXRNwosQu1h5O6iwN3M-N-JG190",
+    authDomain: "watchtogether-a8e28.firebaseapp.com",
+    databaseURL: "https://watchtogether-a8e28-default-rtdb.firebaseio.com/",
+    projectId: "watchtogether-a8e28",
+    storageBucket: "watchtogether-a8e28.firebasestorage.app",
+    messagingSenderId: "393892789141",
+    appId: "1:393892789141:web:b0877f280288d3277a149c"
+  };
+
+ 
